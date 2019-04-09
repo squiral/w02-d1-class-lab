876d7498bb7b635343a8136d9342a6d93c704ae2
Alison Williams <alison.lauriewill@gmail.com>
2019-04-09 08:53:48 +0100

    Getting there with Library Exercise

--- library_class.rb
+++ library_class.rb
@@ -1,56 +1,38 @@
 class Library
 
-  lord_of_the_rings = {
-    title: "Lord of the Rings",
-    rental_details: {
-      student_name: "Jeff",
-      date: "01/12/16"
+  attr_accessor :books
+
+  def initialize()
+    @books = [
+     {
+      title: "Lord of the Rings",
+      rental_details: {
+        student_name: "Jeff",
+        date: "01/12/16"
+      }
+     },
+
+     {
+      title: "Alice in Wonderland",
+      rental_details: {
+        student_name: "Jessica",
+        date: "01/12/17"
+      }
+    },
+    {
+      title: "Lonely Planet: Korea",
+      rental_details: {
+        student_name: "Molly",
+        date: "25/02/18"
+      }
     }
-  }
+  ]
+end
 
-  alice_in_wonderland = {
-    title: "Alice in Wonderland",
-    rental_details: {
-      student_name: "Jessica",
-      date: "01/12/17"
-    }
-  }
-
-  lonely_planet = {
-    title: "Lonely Planet: Korea",
-    rental_details: {
-      student_name: "Molly",
-      date: "25/02/18"
-    }
-  }
-
-  def initialize(books)
-    @books = books
+  def book_info(title)
+    for book in @books
+      return book if book[:title] == title
+    end
   end
-
-
-  def books
-    return @books
-  end
-
-  def get_book_information(book)
-    return "Title: #{book[:title]}, Student Name: #{book[:rental_details][:student_name]}, Date: #{book[:rental_details][:date]}"
-  end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 end
